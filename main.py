8fcc376533bc862df50497302bc6b6f35345a111
CalvinYan <calviny524@gmail.com>
2019-05-25 19:12:50 -0700

    Replace timer-based with listener-based method using user32.dll

--- main.py
+++ main.py
@@ -1,23 +1,71 @@
-# Productivize runs as a background desktop app, checking the name of the foreground application
-# every second, and generating a report of the distribution of time spent using each application
-# on command.
-
-import win32
-import win32gui, win32process
+import sys
 import time
-import psutil
+import ctypes
+import ctypes.wintypes
 
-# Time spent for each app
-time_dict = {}
+EVENT_OBJECT_FOCUS = 0x8005
+WINEVENT_OUTOFCONTEXT = 0x0000
 
-while True:
-    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
-    print(pid)
-    print(psutil.Process(pid[-1]).name())
-    process_name = win32gui.GetWindowText(win32gui.GetForegroundWindow())
-    # print(process_name)
-    if process_name not in time_dict:
-        time_dict[process_name] = 0
-    time_dict[process_name] += 1
-    print(time_dict)
-    time.sleep(1)
+num_updates = 0
+last_time = 0 # integer timestamp of the last change in focus
+
+user32 = ctypes.windll.user32
+ole32 = ctypes.windll.ole32
+
+ole32.CoInitialize(0)
+
+WinEventProcType = ctypes.WINFUNCTYPE(
+    None, 
+    ctypes.wintypes.HANDLE,
+    ctypes.wintypes.DWORD,
+    ctypes.wintypes.HWND,
+    ctypes.wintypes.LONG,
+    ctypes.wintypes.LONG,
+    ctypes.wintypes.DWORD,
+    ctypes.wintypes.DWORD
+)
+
+def callback(hWinEventHook, event, hwnd, idObject, idChild, dwEventThread, dwmsEventTime):
+    global num_updates
+    global last_time
+    length = user32.GetWindowTextLengthA(hwnd)
+    buff = ctypes.create_string_buffer(length + 1)
+    user32.GetWindowTextA(hwnd, buff, length + 1)
+    name = str(buff.value, 'windows-1252')
+    if name != '':
+        new_time = int(time.time())
+        print(time.strftime('%X') + ' --> ' + name)
+        if (last_time != 0):
+            print(str(new_time - last_time) + ' seconds have elapsed')
+        last_time = new_time
+        if num_updates == 30: quit()
+        num_updates += 1
+
+WinEventProc = WinEventProcType(callback)
+
+user32.SetWinEventHook.restype = ctypes.wintypes.HANDLE
+hook = user32.SetWinEventHook(
+    EVENT_OBJECT_FOCUS,
+    EVENT_OBJECT_FOCUS,
+    0,
+    WinEventProc,
+    0,
+    0,
+    WINEVENT_OUTOFCONTEXT
+)
+
+if hook == 0:
+    print('SetWinEventHook failed')
+    sys.exit(1)
+
+msg = ctypes.wintypes.MSG()
+
+counter = 0
+while user32.GetMessageW(ctypes.byref(msg), 0, 0, 0) != 0:
+    user32.TranslateMessageW(msg)
+    user32.DispatchMessageW(msg)
+
+
+
+user32.UnhookWinEvent(hook)
+ole32.CoUninitialize()
